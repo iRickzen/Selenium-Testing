682b2171a614562bc26ee199f8973b59f78d430b
iRickzen <irickzen@gmail.com>
2017-10-27 23:36:08 +0200

    added small example of selenium driver using docker-container

--- selenium_docker.py
+++ selenium_docker.py
@@ -1,29 +1,49 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
 import docker
 
 
-#client = docker.from_env()
-#container = client.containers.run("selenium/standalone-chrome:3.6.0-copper", detach=True, network="4444:4444")
-#print(container)
-#container = client.containers.get(container_id)
+### For debugging purpose use Real vnc and connect on localhost:5900
+### check if container allready exists - only one standalone at a time is possible
+docker_img = "selenium/standalone-chrome-debug:3.6.0-copper"
+client = docker.from_env()
+container = client.containers.list(filters={"ancestor":docker_img})
+
+if container:  container = container[0]
+else:
+    container = client.containers.run(docker_img, detach=True, ports={'4444':'4444','5900':'5900'})
+
+retries = 5 #try max 5 seconds in case container needs some time to start
+while retries >= 0 and container.status != 'running':
+    retries -= 1
+    time.sleep(1)
+### Selenium webdriver in docker-container ready to go
 
 
-
+# get Selenium remote driver
 driver = webdriver.Remote('http://127.0.0.1:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
 
 
 driver.get('http://www.google.com/xhtml');
-time.sleep(5) # Let the user actually see something!
+time.sleep(2) # delay to watch screen
 search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
+
+search_box.send_keys('10+46')
+time.sleep(1) # delay to watch screen
 search_box.submit()
-time.sleep(5) # Let the user actually see something!
+time.sleep(1) # delay to watch screen
+
+calc_box = driver.find_element_by_id('cwos')
+
+### finally some real testing
+#print(calc_box.get_attribute("innerHTML"))
+assert int(calc_box.get_attribute("innerHTML")) == 56
+
+
 driver.quit()
-
-
-
-#container.stop()
+# stop container
+container.stop()
 
 
